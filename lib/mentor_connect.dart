597ca91aa9a236a7ffc7e299037c768ecf447337
pranaav kumar <pranaavkumarj@gmail.com>
2025-03-27 10:55:25 +0530

    files updated

---
+++ lib/mentor_connect.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MentorConnectPage extends StatelessWidget {
+  const MentorConnectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Forum Page - Discuss topics with peers and alumni'),
+    );
+  }
+}
